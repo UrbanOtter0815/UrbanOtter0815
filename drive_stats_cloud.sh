ac3e0ebb6799caccbbbc2630da5fa86475407bfc
Urban Otter <stephan@heistestumpf.de>
2025-11-17 14:31:34 +0100

    Update drive_stats_cloud.sh

--- drive_stats_cloud.sh
+++ drive_stats_cloud.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "\033[37mCloud stats:\033[0m"
-# du -sh /Users/sstumpf/myCloud/*
+# du -sh $HOME/myCloud/*
 du -sh ~/Library/Mobile\ Documents/com~apple~CloudDocs/
 du -sh ~/myClouds/NextCloud/
 du -sh ~/myClouds/Oracle\ Corporation/
