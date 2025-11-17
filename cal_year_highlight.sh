9057dc34517d4540152429d576efb66cad81b8ef
Urban Otter <stephan@heistestumpf.de>
2025-11-17 14:30:59 +0100

    Update cal_year_highlight.sh

--- cal_year_highlight.sh
+++ cal_year_highlight.sh
@@ -7,8 +7,8 @@
 #               works in standard shells supporting awk and cal.
 # Usage       : ./highlight-current-day-calendar.sh
 # Arguments   : None
-# Author      : [Your Name]
-# Date        : [YYYY-MM-DD]
+# Author      : Stephan Stumpf
+# Date        : 2025-11-17
 # Version     : 1.0
 # previous command: cal -y | awk -v month="`date +%m`" -v day="`date +%e` " '{m=int((NR-3)/8)*3+1; for (i=0;i<3;i++) {t[i]=substr($0,1+i*22,20) " "; if (m+i==month) sub(day,"\033[0;31m&\033[0m",t[i]);} print t[0],t[1],t[2];}'
 ###############################################################################
